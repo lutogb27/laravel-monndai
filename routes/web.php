37e1a00030983fd80731efe280f6aa95c7d0c0ee
lutogb27 <lutogb27@yahoo.co.jp>
2022-02-23 10:25:04 +0900

    first-commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Middleware\FirstMiddleware;
+
+Route::get('/', [TestController::class, 'index']);
+Route::post('/', [TestController::class, 'post']);
